afa17aa45c414c957bdbf908db95006583a528ab
Nichapa P <nchncp@gmail.com>
2016-09-12 15:57:51 +0700

    Add Popup Window

--- app/src/main/java/com/example/elderly/simplelauncher/HomeActivity.java
+++ app/src/main/java/com/example/elderly/simplelauncher/HomeActivity.java
@@ -6,7 +6,6 @@ import android.app.Activity;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
-import android.content.ComponentName;
 import android.widget.Button;
 
 
---
+++ app/src/main/java/com/example/elderly/simplelauncher/MediaController.java
@@ -0,0 +1,7 @@
+package com.example.elderly.simplelauncher;
+
+/**
+ * Created by nicha on 9/12/16.
+ */
+public class MediaController {
+}
