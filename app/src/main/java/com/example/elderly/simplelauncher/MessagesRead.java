db3784ac79ccae27b0665850681847f613e47e22
Nichapa P <nchncp@gmail.com>
2016-09-25 05:53:28 +0700

    Add sorting sms

--- app/src/main/java/com/example/elderly/simplelauncher/MessagesRead.java
+++ app/src/main/java/com/example/elderly/simplelauncher/MessagesRead.java
@@ -32,7 +32,7 @@ public class MessagesRead extends Activity {
     public interface APIService {
 //      @GET("stayintouch/WebApplication/examples/web/json/readMessage.php?accountid={accountid}")
 //      Call<List<MessagesModel>> getMessage(@Path("accountid") String accountid);
-        @GET("stayintouch/WebApplication/examples/web/json/readMessage.php?accountid=54270371")
+        @GET("el_launcher/unreadMessages.php?accountid=54270371")
         Call<List<MessagesModel>> getMessage();
     }
 
