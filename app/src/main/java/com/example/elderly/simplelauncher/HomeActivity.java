ef69ecc3275b4029a17ccba476663ab9973c65e4
Nichapa P <nchncp@gmail.com>
2016-09-27 05:06:35 +0700

    Hide popup button

--- app/src/main/java/com/example/elderly/simplelauncher/HomeActivity.java
+++ app/src/main/java/com/example/elderly/simplelauncher/HomeActivity.java
@@ -72,7 +72,7 @@ public class HomeActivity extends Activity {
         notiTopic = (TextView)findViewById(R.id.noti_title);
         notiDetail = (TextView)findViewById(R.id.noti_detail);
 
-        popupWindow();
+        //popupWindow();
 
     }
 
@@ -110,76 +110,74 @@ public class HomeActivity extends Activity {
             tFName.setText("-");
             tLName.setText("-");
         }
-
-
-
+        
     }
 
-    public void popupWindow() {
-
-        // Get the application context
-        mContext = getApplicationContext();
-
-        // Get the activity
-        mActivity = HomeActivity.this;
-
-        // Get the widgets reference from XML layout
-        mRelativeLayout = (RelativeLayout) findViewById(R.id.layout_home);
-        mButton = (Button) findViewById(R.id.btnPopup);
-
-        // Set a click listener for the text view
-        mButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                // Initialize a new instance of LayoutInflater service
-                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
-
-                // Inflate the custom layout/view
-                View customView = inflater.inflate(R.layout.notification,null);
-
-                mPopupWindow = new PopupWindow(
-                        customView,
-                        LayoutParams.WRAP_CONTENT,
-                        LayoutParams.WRAP_CONTENT
-                );
-
-                // Set an elevation value for popup window
-                // Call requires API level 21
-                if(Build.VERSION.SDK_INT>=21){
-                    mPopupWindow.setElevation(5.0f);
-                }
-
-                // Get a reference for the custom view close button
-                Button okayButton = (Button) customView.findViewById(R.id.noti_close);
-
-                // Set a click listener for the popup window close button
-                okayButton.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View view) {
-                        // Dismiss the popup window
-                        mPopupWindow.dismiss();
-                    }
-                });
-
-                /*
-                    public void showAtLocation (View parent, int gravity, int x, int y)
-                        Display the content view in a popup window at the specified location. If the
-                        popup window cannot fit on screen, it will be clipped.
-                        Learn WindowManager.LayoutParams for more information on how gravity and the x
-                        and y parameters are related. Specifying a gravity of NO_GRAVITY is similar
-                        to specifying Gravity.LEFT | Gravity.TOP.
-
-                    Parameters
-                        parent : a parent view to get the getWindowToken() token from
-                        gravity : the gravity which controls the placement of the popup window
-                        x : the popup's x location offset
-                        y : the popup's y location offset
-                */
-                // Finally, show the popup window at the center location of root relative layout
-                mPopupWindow.showAtLocation(mRelativeLayout, Gravity.CENTER,0,0);
-            }
-        });
-    }
+//    public void popupWindow() {
+//
+//        // Get the application context
+//        mContext = getApplicationContext();
+//
+//        // Get the activity
+//        mActivity = HomeActivity.this;
+//
+//        // Get the widgets reference from XML layout
+//        mRelativeLayout = (RelativeLayout) findViewById(R.id.layout_home);
+//        mButton = (Button) findViewById(R.id.btnPopup);
+//
+//        // Set a click listener for the text view
+//        mButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View view) {
+//                // Initialize a new instance of LayoutInflater service
+//                LayoutInflater inflater = (LayoutInflater) mContext.getSystemService(LAYOUT_INFLATER_SERVICE);
+//
+//                // Inflate the custom layout/view
+//                View customView = inflater.inflate(R.layout.notification,null);
+//
+//                mPopupWindow = new PopupWindow(
+//                        customView,
+//                        LayoutParams.WRAP_CONTENT,
+//                        LayoutParams.WRAP_CONTENT
+//                );
+//
+//                // Set an elevation value for popup window
+//                // Call requires API level 21
+//                if(Build.VERSION.SDK_INT>=21){
+//                    mPopupWindow.setElevation(5.0f);
+//                }
+//
+//                // Get a reference for the custom view close button
+//                Button okayButton = (Button) customView.findViewById(R.id.noti_close);
+//
+//                // Set a click listener for the popup window close button
+//                okayButton.setOnClickListener(new View.OnClickListener() {
+//                    @Override
+//                    public void onClick(View view) {
+//                        // Dismiss the popup window
+//                        mPopupWindow.dismiss();
+//                    }
+//                });
+//
+//                /*
+//                    public void showAtLocation (View parent, int gravity, int x, int y)
+//                        Display the content view in a popup window at the specified location. If the
+//                        popup window cannot fit on screen, it will be clipped.
+//                        Learn WindowManager.LayoutParams for more information on how gravity and the x
+//                        and y parameters are related. Specifying a gravity of NO_GRAVITY is similar
+//                        to specifying Gravity.LEFT | Gravity.TOP.
+//
+//                    Parameters
+//                        parent : a parent view to get the getWindowToken() token from
+//                        gravity : the gravity which controls the placement of the popup window
+//                        x : the popup's x location offset
+//                        y : the popup's y location offset
+//                */
+//                // Finally, show the popup window at the center location of root relative layout
+//                mPopupWindow.showAtLocation(mRelativeLayout, Gravity.CENTER,0,0);
+//            }
+//        });
+//    }
 
     public void showLogin(View v) {
         Intent i = new Intent(this, LoginActivity.class);
